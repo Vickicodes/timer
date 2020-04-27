f0fc638a4678cec512f2db7b50a09f712c22a0cf
Victoria Hawoldar <victoria@hawoldar.co.uk>
2020-04-27 10:46:35 +0100

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const durationInput = document.querySelector('#duration');
+const startButton = document.querySelector('#start');
+const pauseButton = document.querySelector('#pause');
+
+const timer = new Timer(durationInput, startButton, pauseButton, {
+	onStart() {
+		console.log('timer started');
+	},
+	onTick() {
+		console.log('timer just ticked down');
+	},
+	onComplete() {
+		console.log('timer is complete');
+	}
+});
